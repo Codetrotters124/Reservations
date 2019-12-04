8b09a9a0fd320899daa9ac413840d16ca4838669
Hien Phuong <hien@MacBook-Pro-3.lan>
2019-12-03 18:42:19 -0800

    Roughly finished the party size component

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import PartySize from './PartySize.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.gettingPartySize = this.gettingPartySize.bind(this);
+  }
+  gettingPartySize(size) {
+    console.log('Hi Im from App', size);
   }
   render() {
     return (
       <div>
-        <h1>Reservations</h1>
-        <h2>testing</h2>
+        <h1>Make a reservation</h1>
+        <PartySize gettingPartySize={this.gettingPartySize}/>
       </div>
     );
   }
