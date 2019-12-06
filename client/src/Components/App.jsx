9372c9792bbab23efb6bdfcb8bf9fc72f8186a84
Hien Phuong <hien@MacBook-Pro-3.local>
2019-12-05 20:53:14 -0800

    Change select style for App, Button, Partysize, Time, and Date

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -18,6 +18,44 @@ const H1 = styled.h3`
   font-size: 21px;
   border-bottom: 1px solid #d8d9db; 
 `;
+const Select = styled.select`
+  cursor: pointer;
+  font-family: inherit;
+  background-color: #fff;
+  font-size: .875rem;
+  display: block;
+  outline: none;
+  border: none;
+  width: 100%;
+  height: 35px;
+  -webkit-appearance: none;
+  border-radius: 0;
+  border-bottom: 1px solid #d8d9db;
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-indent: 0px;
+  text-shadow: none;
+  text-align: start;
+  box-sizing: border-box;
+  align-items: center;
+  white-space: pre;
+  -webkit-rtl-ordering: logical;
+  background-image:
+    linear-gradient(45deg, transparent 50%, gray 50%),
+    linear-gradient(135deg, gray 50%, transparent 50%);
+    // linear-gradient(to right, #ccc, #ccc);
+  background-position:
+    calc(100% - 20px) calc(1em + 2px),
+    calc(100% - 15px) calc(1em + 2px);
+    // calc(100% - 2.5em) 0.5em;
+  background-size:
+    5px 5px,
+    5px 5px,
+    1px 1.5em;
+  background-repeat: no-repeat;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -31,9 +69,9 @@ class App extends React.Component {
     return (
       <Div>
         <H1>Make a reservation</H1>
-        <PartySize gettingPartySize={this.gettingPartySize}/>
-        <Time />
-        <Date />
+        <PartySize Select={Select} gettingPartySize={this.gettingPartySize}/>
+        <Time Select={Select} />
+        <Date Select={Select}/>
         <FindTable />
       </Div>
     );
