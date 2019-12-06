d844b8ad3f68dc4b5b2e68df77c614002e9dc35d
Hien Phuong <hien@MacBook-Pro-3.local>
2019-12-05 17:09:44 -0800

    Put app into a box

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,6 +3,22 @@ import PartySize from './PartySize.jsx';
 import Time from './Time.jsx';
 import FindTable from './FindTable.jsx';
 import Date from './Date.jsx';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  width: 300px;
+  border: 1px solid white;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.3);
+  padding: 50px;
+  margin: 20px;
+`;
+
+const H1 = styled.h3`
+  font-family: Oswald;
+  font-size: 21px;
+  // text-decoration-line: underline;
+  border-bottom: 1px solid #d8d9db; 
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -14,13 +30,13 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1>Make a reservation</h1>
+      <Div>
+        <H1>Make a reservation</H1>
         <PartySize gettingPartySize={this.gettingPartySize}/>
         <Time />
         <Date />
         <FindTable />
-      </div>
+      </Div>
     );
   }
 }
